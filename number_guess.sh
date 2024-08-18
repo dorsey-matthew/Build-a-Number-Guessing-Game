1d6ceb30e50a7ae80bf6545b39c08e6414f0d75f
matthew dorsey <matthewthomasdorsey@gmail.com>
2024-08-18 18:12:30 +0000

    fix:

--- number_guess.sh
+++ number_guess.sh
@@ -1,13 +1,45 @@
 #!/bin/bash
 PSQL="psql --username=freecodecamp --dbname=number_guess -t --no-align -c"
+secret=$(($RANDOM % 1000 + 1))
 echo "Enter your username:"
 read username
 old_user=$($PSQL "select username from users where username=$username")
 if [[ -z $old_user ]]
 then
 new_user=$($PSQL "insert into users (username) values('$username')")
+echo "Welcome, $username! It looks like this is your first time here."
+
+
 else
 games_played=$($PSQL "select count(*) from games inner join using(user_id) where user_id=$username"
 best_game=$($PSQL "select min(number_of_guesses) from games inner join using(user_id) where user_id=$username"
 echo "Welcome back, $username! You have played $games_played games, and your best game took $best_game guesses."
-echo $(($RANDOM % 1000 + 1))
+
+fi
+
+
+
+
+echo "Guess the secret number between 1 and 1000"
+read guess
+while [ ! $guess -e $secret ]
+do
+
+
+
+
+if [[ ! $guess =~ ^[0-9]+$ ]]
+then
+echo "That is not an integer, guess again:"
+else
+if [[ guess -gt $secret ]]
+then
+echo "It's higher than that, guess again:"
+elif [[ guess -lt $secret ]]
+then
+echo "It's lower than that, guess again:"
+else
+fi
+fi
+
+done
