81eb7802eacd68193903a4892789dc54e38a5de2
matthew dorsey <matthewthomasdorsey@gmail.com>
2024-08-18 19:18:50 +0000

    feat: almost done

--- number_guess.sh
+++ number_guess.sh
@@ -9,22 +9,22 @@ echo "Welcome, $username! It looks like this is your first time here."
 new_user=$($PSQL "insert into users (username,games_played) values('$username',0)")
 else
 games_played=$($PSQL "select games_played from users where username='$username'")
-best_game=$($PSQL "select best_game from users from users where username='$username'")
+best_game=$($PSQL "select best_game from users where username='$username'")
 echo "Welcome back, $username! You have played $games_played games, and your best game took $best_game guesses."
 fi
 secret=$(( RANDOM % 1000 + 1 ))
 echo "Guess the secret number between 1 and 1000"
-guess=0
+guesses=0
 while read guess
 do
 if [[ ! $guess =~ ^[0-9]+$ ]]
 then
 echo "That is not an integer, guess again:"
 else
-((guess++))
+((guesses++))
 if [[ $guess -eq $secret ]]
 then
-echo "You guessed it in $guess tries. The secret number was $secret. Nice job!"
+echo "You guessed it in $guesses tries. The secret number was $secret. Nice job!"
 break
 elif [[ $guess -lt $secret ]]
 then
@@ -36,11 +36,11 @@ fi
 done
 if [[ -z $old_user ]]
 then
-update=$($PSQL "update users set games_played = 1,best_game=$guess where username='$username'")
+update=$($PSQL "update users set games_played = 1,best_game=$guesses where username='$username'")
 else
 update=$($PSQL "update users set games_played = games_played + 1 where username='$username'")
-if [[ $guess -lt $best_game || -z $best_game ]]
+if [[ $best_game -eq "null" || $guesses -lt $best_game ]]
 then
-update=$($PSQL "update users set best_game = $guess where username='$username'")
+update=$($PSQL "update users set best_game = $guesses where username='$username'")
 fi
 fi
